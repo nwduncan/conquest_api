ebab2e4630ecdf3a83b7ab1effbc8cf2a29a76b4
nwduncan <nduncan.au@gmail.com>
2019-01-14 11:22:40 +1100

    Folder restructure

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='conquest_api',
       install_requires=['requests'],
       version='0.9',
       license='MIT',
-      description='A Python wrapper for working with the Conquest API',
+      description='A Python wrapper for working with the Conquest API'
 )
