52aa1d914eca54d46e196f13dd295fab5dbb3e06
nwduncan <nduncan.au@gmail.com>
2019-01-14 11:43:36 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='conquest_api',
       url='https://github.com/nwduncan/conquest_api',
       author='Nathan Duncan',
+      packages=['conquest_api'],
       author_email='nduncan.au@gmail.com',
       install_requires=['requests'],
       version='0.9',
